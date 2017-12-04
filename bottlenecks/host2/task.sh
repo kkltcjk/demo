d952c8f8dcad872dcb9e88b8b7909ee6b608fe92
chenjiankun <chenjiankun1@huawei.com>
2017-12-03 22:44:20 -0800

    for 114.2

--- bottlenecks/host2/task.sh
+++ bottlenecks/host2/task.sh
@@ -1,1 +1,1 @@
-docker run -itd --privileged -v /var/run/docker.sock:/var/run/docker.sock -p 8888:5000 -v /home/kklt/os/pod.yaml:/etc/yardstick/pod.yaml -v /home/kklt/tmp:/tmp/yardstick/ -e CONDUCTOR=http://192.168.23.10:8888/api --name task kkltcjk/yardstick:task
+docker run -itd --privileged -v /var/run/docker.sock:/var/run/docker.sock -p 8888:5000 -v /home/kklt/os/pod.yaml:/etc/yardstick/pod.yaml -v /home/kklt/tmp:/tmp/yardstick/ -e CONDUCTOR=http://192.168.114.10:8888/api --name task kkltcjk/yardstick:task
